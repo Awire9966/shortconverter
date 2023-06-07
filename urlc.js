a8bc2d4f72bcab820fa7357961be2abaa3f025eb
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2023-06-06 21:09:29 -0400

    Converter

--- urlc.js
+++ urlc.js
@@ -1,7 +1,7 @@
 function urlc()
 {
     var urlbox = document.getElementById("url");
-    var urlarr = urlbox.split("/");
+    var urlarr = urlbox.value.split("/");
     var vid = urlarr[urlarr.length - 1];
     urlbox.value = "youtube.com/watch?v=" + vid.ToString();
 }
